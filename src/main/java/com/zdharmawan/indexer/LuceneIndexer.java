57523b17aa021fd5c26ee4dbfa0d78d100a79772
Zulfikar Dharmawan <zulfikar.dharmawan@gmail.com>
2014-06-05 15:15:11 +0200

    Preparing creating an indexer

--- src/main/java/com/zdharmawan/indexer/LuceneIndexer.java
+++ src/main/java/com/zdharmawan/indexer/LuceneIndexer.java
@@ -1,4 +1,4 @@
-package com.zdharmawan.search;
+package com.zdharmawan.indexer;
 
 import java.io.File;
 
@@ -9,24 +9,27 @@ import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LuceneIndexer {
 
     private static final String pathToIndex = "";
+    private static final Logger logger = LoggerFactory.getLogger(LuceneIndexer.class);
     
     public boolean openIndex() {
         IndexWriter indexWriter = null;
         try {
             Directory dir = FSDirectory.open(new File(pathToIndex));
             Analyzer analyzer = new SimpleAnalyzer(Version.LUCENE_48);
-
             IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_48, analyzer);
+
             iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
             indexWriter = new IndexWriter(dir, iwc);
             
             return true;
         } catch (Exception e) {
-            System.out.println("Threw an exception trying to open the index for writing: " + e.getClass() + " :: " + e.getMessage());
+            logger.warn("Threw an exception trying to open the index for writing: " + e.getClass() + " :: " + e.getMessage());
             return false;
         }
     }
